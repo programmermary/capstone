e352845185288ef7bfbdfd7a8233f991bf14435a
nura <nooraliabdalhossen@gmail.com>
2024-05-24 19:32:56 +0300

    release border

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -4,7 +4,7 @@ import service from '../assets/service.png'
 import ourserive from '../assets/ourserive.png'
 export default function Cards() {
     return (
-        <div className='  w-70 bg-black-100  mx-20 my-20 '>
+        <div className='  w-70 bg-black-100  mx-20 my-10  '>
 
             <div className='h-20   rounded-xl mt-10 flex bg-slate-100 shadow-lg shadow-gray-500/30  justify-center	'> 
             <h1 className=' text-center mt-5'>خدماتنا</h1>
