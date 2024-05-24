c5732b2a97d54c758b68ee7d634ed86e525791eb
unknown <maryamihsan601@gmail.com>
2024-05-24 15:39:20 +0300

    add tailwind css

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,6 +6,7 @@ import './styles/HomeStyle.css'
 export default function Home() {
   return (
     <div className='home'>
+      <h1 className='text-7xl text-center text-blue-300'>hello</h1>
       <img className='homeSvg'src={homeSvg}/>
       <button className='btn'>حالة طارىء<i class="fa-solid fa-arrow-right"></i></button>
     </div>
