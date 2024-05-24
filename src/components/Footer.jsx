7433ba2d627414ad276025205f4204e49bee9412
nura <nooraliabdalhossen@gmail.com>
2024-05-24 17:33:59 +0300

    add footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,10 +4,10 @@ import user from '../assets/user.png'
 import brightness from '../assets/brightness.png'
 export default function Footer() {
   return (
-    <div className='border-transparent  bg-slate-100 w-screen h-20 flex  justify-between'>
-        <img className='h-10  ml-6'src={user}/>
-        <img className='h-10'src={homePage}/>
-        <img className='h-10 mr-6'src={brightness}/>
+    <div className='  bg-slate-100 w-screen h-20 flex  justify-between'>
+        <img className='h-10  ml-6 p-1'src={user}/>
+        <img className='h-10 p-1'src={homePage}/>
+        <img className='h-10 mr-6 p-1'src={brightness}/>
     </div>
   )
 }
