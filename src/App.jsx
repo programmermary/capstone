c0768c978a3deb112f34b422792ec3d07b19c9e2
nura <nooraliabdalhossen@gmail.com>
2024-05-24 17:12:36 +0300

    add footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import Footer from "./components/Footer";
+
 import Home from "./components/Home";
 import './MainStyle.css'
 function App() {
   return (
    <div className="main-app">
      <Home/>
-    <Footer/>
+    
    </div>
   );
 }
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
+import Footer from "../components/Footer";
 import homeSvg from '../assets/homeSvg.svg'
-import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
 import './styles/HomeStyle.css'
 
 export default function Home() {
@@ -9,6 +9,7 @@ export default function Home() {
       <h1 className='text-7xl text-center text-blue-300'>hello</h1>
       <img className='homeSvg'src={homeSvg}/>
       <button className='btn'>حالة طارىء<i class="fa-solid fa-arrow-right"></i></button>
+      <Footer/>
     </div>
   )
 }
