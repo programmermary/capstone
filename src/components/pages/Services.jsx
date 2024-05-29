c60b385cb801a628c5e453de72e48fdce7b0b7ee
nura <nooraliabdalhossen@gmail.com>
2024-05-29 16:29:32 +0300

    add cards

--- src/components/pages/Services.jsx
+++ src/components/pages/Services.jsx
@@ -3,7 +3,59 @@ import React from 'react'
 export default function Services() {
   return (
     <div>
-        <h1>this is Services file </h1>
+         <div className='bg-slate-100 gap-5 grid grid-cols-2 mt-5 '>
+
+
+<div className='bg-slate-200 w-40 h-48 rounded-2xl  shadow-lg shadow-gray-500/30  '>
+            
+        <div className='bg-slate-100 rounded-2xl w-28 h-24 mt-2 mr-5 '>
+           
+        </div>
+        <h3 className='mr-8 text-sm '>الاسم   </h3>
+
+         <h3 className='mr-8 text-xs '>السعر 10 الاف  </h3>
+         <button className='bg-red-300 rounded-sm w-20 mr-8 text-xs '> احجز هنا  </button>
+
+ </div>
+ <div className='bg-slate-200 w-40 h-48 rounded-2xl  shadow-lg shadow-gray-500/30  '>
+            
+        <div className='bg-slate-100 rounded-2xl w-28 h-28 mt-2 mr-5 '>
+           
+        </div>
+        <h3 className='mr-8 text-sm '>الاسم   </h3>
+
+         <h3 className='mr-8 text-xs '>السعر 10 الاف  </h3>
+         <button className='bg-red-300 rounded-sm w-20 mr-8 text-xs '> احجز هنا  </button>
+
+ </div>
+ <div className='bg-slate-200 w-40 h-48 rounded-2xl  shadow-lg shadow-gray-500/30  '>
+            
+        <div className='bg-slate-100 rounded-2xl w-28 h-28 mt-2 mr-5 '>
+           
+        </div>
+        <h3 className='mr-8 text-sm '>الاسم   </h3>
+
+         <h3 className='mr-8 text-xs '>السعر 10 الاف  </h3>
+         <button className='bg-red-300 rounded-sm w-20 mr-8 text-xs '> احجز هنا  </button>
+
+ </div>
+ <div className='bg-slate-200 w-40 h-48 rounded-2xl  shadow-lg shadow-gray-500/30  '>
+            
+        <div className='bg-slate-100 rounded-2xl w-28 h-28 mt-2 mr-5 '>
+           
+        </div>
+        <h3 className='mr-8 text-sm '>الاسم   </h3>
+
+         <h3 className='mr-8 text-xs '>السعر 10 الاف  </h3>
+         <button className='bg-red-300 rounded-sm w-20 mr-8 text-xs '> احجز هنا  </button>
+
+ </div>
+ 
+ 
+
+ 
+
+</div>
     </div>
   )
 }
