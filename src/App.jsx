cdab0a8fc912048dee594f35bd2bdfa6356751a0
nura <nooraliabdalhossen@gmail.com>
2024-05-29 16:33:02 +0300

    add router to team file

--- src/App.jsx
+++ src/App.jsx
@@ -4,23 +4,27 @@ import Footer from "./components/Footer";
 import Home from './components/Home'
 import DailyCare from "./components/pages/DailyCare";
 import Services from "./components/pages/Services";
+import Team from "./components/pages/Team";
 import AppRutes from './AppRutes';
+
 import { BrowserRouter, Route,  Routes } from 'react-router-dom';
 function App() {
   return (
    <div className="main-app">
+  
      {/* <Emergency/>
     <Footer/>
      <Home/> */}
 
          <BrowserRouter>
          <AppRutes/>
-      {/* <Routes>
+      {<Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/services' element={<Services/>}/>
+        <Route path='/team' element={<Team/>}/>
         <Route path="/dailycare" element={<DailyCare/>}/>
         <Route path="/emergency" element={<Emergency/>}/>
-      </Routes> */}
+    </Routes>}
       </BrowserRouter>
    </div>
   );
