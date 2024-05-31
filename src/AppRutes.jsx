6ed4c1b6266a5d7e4542f71eb77a727aec8b5050
nura <nooraliabdalhossen@gmail.com>
2024-05-31 21:21:15 +0300

    add route to Team

--- src/AppRutes.jsx
+++ src/AppRutes.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import DoneEmrgencey from './components/DoneEmrgencey'
 import Home from './components/Home'
 import Services from './components/pages/Services'
+import Team from "./components/pages/Team";
 import DailyCare from './components/pages/DailyCare'
 import Emergency from './components/Emergency'
 export default function AppRutes() {
@@ -13,6 +14,7 @@ export default function AppRutes() {
       <Route path='/' element={<Home/>}/>
         <Route path='/services' element={<Services/>}/>
         <Route path="/dailycare" element={<DailyCare/>}/>
+        <Route path='/team' element={<Team/>}/>
         <Route path="/emergency" element={<Emergency/>}/>
         <Route path='/doneEmergency' element={<DoneEmrgencey/>}/>
       </Routes>
