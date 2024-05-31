6c3a8f5b49eef0ddaa6e3bdbb672a294c89e0d0f
nura <nooraliabdalhossen@gmail.com>
2024-05-31 21:18:06 +0300

    add images

--- src/components/pages/Team.jsx
+++ src/components/pages/Team.jsx
@@ -5,6 +5,7 @@ export default function Team() {
     <div className="bg-slate-100">
       <h1 className="text-center mt-10"> فرقنا </h1>
       <div className="bg-slate-100 gap-5 grid grid-cols-1 mt-5 ">
+      
         <div className="bg-slate-200 w-64 h-32 rounded-2xl  shadow-lg shadow-gray-500/30 mr-10 ">
           <div className="bg-slate-100 rounded-2xl w-24 h-24 mt-2 mr-20 "></div>
           <h3 className="mr-8 text-sm  mb-2">الاسم </h3>
