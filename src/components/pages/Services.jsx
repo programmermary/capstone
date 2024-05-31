d1e29ec2a770a20e0f94d00c149da212169ed136
nura <nooraliabdalhossen@gmail.com>
2024-05-31 21:16:41 +0300

    add image

--- src/components/pages/Services.jsx
+++ src/components/pages/Services.jsx
@@ -1,14 +1,29 @@
 import React from 'react'
+import cannula from '../../assets/cannula.png';
+import sugar from '../../assets/sugar.png'
+import bloodPressure from '../../assets/blood-pressure.png'
+
+import injection from '../../assets/injection.png'
+
+import hand from '../../assets/hand.png'
+
+import suture from '../../assets/suture.png'
+
+
+
+
+
+
 
 export default function Services() {
   return (
     <div>
          <div className='bg-slate-100 gap-5 grid grid-cols-2 mt-5 '>
 
-
 <div className='bg-slate-200 w-40 h-48 rounded-2xl  shadow-lg shadow-gray-500/30  '>
             
         <div className='bg-slate-100 rounded-2xl w-28 h-24 mt-2 mr-5 '>
+        <img className=' h-20 w-20 mt-6 mb-20  mr-4' src={suture}/>
            
         </div>
         <h3 className='mr-8 text-sm '>الاسم   </h3>
@@ -20,6 +35,7 @@ export default function Services() {
  <div className='bg-slate-200 w-40 h-48 rounded-2xl  shadow-lg shadow-gray-500/30  '>
             
         <div className='bg-slate-100 rounded-2xl w-28 h-28 mt-2 mr-5 '>
+        <img className=' h-20 w-20 mt-4 mb-20  mr-4'  src={hand}/>
            
         </div>
         <h3 className='mr-8 text-sm '>الاسم   </h3>
@@ -31,6 +47,7 @@ export default function Services() {
  <div className='bg-slate-200 w-40 h-48 rounded-2xl  shadow-lg shadow-gray-500/30  '>
             
         <div className='bg-slate-100 rounded-2xl w-28 h-28 mt-2 mr-5 '>
+        <img className=' h-20 w-20 mt-4 mb-20  mr-4' src={sugar}/>
            
         </div>
         <h3 className='mr-8 text-sm '>الاسم   </h3>
@@ -42,6 +59,7 @@ export default function Services() {
  <div className='bg-slate-200 w-40 h-48 rounded-2xl  shadow-lg shadow-gray-500/30  '>
             
         <div className='bg-slate-100 rounded-2xl w-28 h-28 mt-2 mr-5 '>
+        <img className=' h-20 w-20 mt-4 mb-20  mr-4' src={bloodPressure}/>
            
         </div>
         <h3 className='mr-8 text-sm '>الاسم   </h3>
@@ -50,6 +68,32 @@ export default function Services() {
          <button className='bg-red-300 rounded-sm w-20 mr-8 text-xs '> احجز هنا  </button>
 
  </div>
+ <div className='bg-slate-200 w-40 h-48 rounded-2xl  shadow-lg shadow-gray-500/30  '>
+            
+            <div className='bg-slate-100 rounded-2xl w-28 h-28 mt-2 mr-5 '>
+            <img className=' h-20 w-20 mt-4 mb-20  mr-4' src={injection}/>
+               
+            </div>
+            <h3 className='mr-8 text-sm '>الاسم   </h3>
+    
+             <h3 className='mr-8 text-xs '>السعر 10 الاف  </h3>
+             <button className='bg-red-300 rounded-sm w-20 mr-8 text-xs '> احجز هنا  </button>
+    
+     </div>
+
+  <div className='bg-slate-200 w-40 h-48 rounded-2xl  shadow-lg shadow-gray-500/30  '>
+            
+        <div className='bg-slate-100 rounded-2xl w-28 h-28 mt-2 mr-5 '>
+        <img className=' h-20 w-20 mt-4 mb-20  mr-4' src={cannula}/>
+           
+        </div>
+        <h3 className='mr-8 text-sm '>الاسم   </h3>
+
+         <h3 className='mr-8 text-xs '>السعر 10 الاف  </h3>
+         <button className='bg-red-300 rounded-sm w-20 mr-8 text-xs '> احجز هنا  </button>
+
+ </div>
+     
  
  
 
